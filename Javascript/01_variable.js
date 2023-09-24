9a571d06dd0503f964983ab0d9d8bcc54a5869b5
rahulb395 <rahulb395@gmail.com>
2023-09-24 09:53:15 +0000

    variable lesson done

---
+++ Javascript/01_variable.js
@@ -0,0 +1,19 @@
+var accountId = "1234";
+const emailid = "rahulb395@gmail.com";
+let pass = 1234;
+accountCity = "Jaipur";
+
+let marks;
+
+// emailid = "navin.b@gmail.com"
+
+pass = 321;
+accountId = "4321";
+accountCity = "Ahmedabad";
+console.log(accountId);
+
+console.table([pass,accountId,accountCity,marks])
+
+/* 
+Prefer not to use var becuase of issue in block scope & functional scope
+*/
