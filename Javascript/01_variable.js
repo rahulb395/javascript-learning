b176cae6ef8c4552fed773862b7892e1b37f29e1
rahulb395 <rahulb395@gmail.com>
2023-09-25 15:53:38 +0000

    datatypes

--- Javascript/01_variable.js
+++ Javascript/01_variable.js
@@ -3,7 +3,7 @@ const emailid = "rahulb395@gmail.com";
 let pass = 1234;
 accountCity = "Jaipur";
 
-let marks;
+let marks1;
 
 // emailid = "navin.b@gmail.com"
 
@@ -12,7 +12,7 @@ accountId = "4321";
 accountCity = "Ahmedabad";
 console.log(accountId);
 
-console.table([pass,accountId,accountCity,marks])
+console.table([pass,accountId,accountCity,marks1])
 
 /* 
 Prefer not to use var becuase of issue in block scope & functional scope
---
+++ Javascript/02_datatypes.js
@@ -0,0 +1,19 @@
+// BONUS
+// B = BOLLEAN
+// B = BINGIT
+// O = OBJECT
+// N = NULL => standalone value
+// N = NUMBER
+// S = SYMBOL => unique
+// S = STRING
+// U = UNDEFINED
+
+"use strict"; // this is used for newer version in Javascript
+
+// let alertbox = alert("sdsd");
+
+//  alert(3+3); // we are using nodejs here not browser
+
+// console.log(alertbox);
+
+console.log(typeof 123);
