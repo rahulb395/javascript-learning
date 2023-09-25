9edcd31b219cc5cc1ee5cfcb02b134ff94256d9d
rahulb395 <rahulb395@gmail.com>
2023-09-25 16:18:53 +0000

    conversion of datatypes

---
+++ Javascript/03_conversion-operation.js
@@ -0,0 +1,19 @@
+let score = "50";
+
+// console.log(typeof score);
+
+let convertintonub = Number (score);
+
+// console.log(typeof convertintonub) // if we want to change string to a number use this "Number"
+
+let num = "";
+
+let BOLLEANnum = Boolean (num);
+
+// console.log(BOLLEANnum);
+
+let anyno = 123;
+
+let string = String (anyno)
+
+console.log(typeof string);
