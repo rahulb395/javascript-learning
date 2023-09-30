c1074230815df7aa028fe3420de3f9c223c7ba62
rahulb395 <rahulb395@gmail.com>
2023-09-30 07:47:40 +0000

    comparision done

---
+++ Javascript/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log( 3 > 1);
+// console.log( 3 > 1);
+// console.log( 3 < 1);
+// console.log( 3 >= 1);
+// console.log( 3 === 3);
+// console.log( 3 !== 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 6 == 3);
+// console.log( 3 == 8);
+// console.log( 3 == 8);
+// console.log( 3 == 8);
+// console.log( 3 == 8);
+// console.log( 3 == 8);
+// console.log( 3 == 8);
+console.log( "3" == 3); // == will compare only numbers
+console.log( "3" === 3); // === three equal will check both datatype & Number 
