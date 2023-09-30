0cbf1ffdc76c4ff479efd5d3c252b9d17188bffe
rahulb395 <rahulb395@gmail.com>
2023-09-30 08:32:24 +0000

    datatype-summary done

---
+++ Javascript/05_datatype-summary.js
@@ -0,0 +1,46 @@
+// Two type of data types are there:
+
+// Primitive
+
+// BONUS
+
+// B = BOLLEAN
+// B = BigInt
+// O = OBJECT
+// N = NULL
+// N = NUMBER
+// U = UNDEFINED
+// S = String
+// S = SYMBOL
+
+// NON PRIMITIVE
+
+//FUNCTION
+//OBJECT
+//ARRAY
+
+let person = ["rahul", "neha", "mohan", "komal"]
+let guys = person[0];
+console.log(guys); //ARRAYS
+
+
+let vechile = {
+
+    car: 2,
+    scooty: "sorata",
+    cycle: "leapecyle"
+
+}
+
+console.log(vechile.car); // OBJECT
+
+let score = myfunction (3,4);
+
+function myfunction (a, b) {
+    console.log(a + b);
+} //FUCNTION
+
+//console.log(score);
+
+
+
