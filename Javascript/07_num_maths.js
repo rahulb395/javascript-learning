f2ef549be7f69f0f05ddf9ddd004d6708c4fbf31
rahulb395 <rahulb395@gmail.com>
2023-10-02 07:10:32 +0000

    number maths done

---
+++ Javascript/07_num_maths.js
@@ -0,0 +1,38 @@
+// const score = 500000;
+
+// console.log(score);
+
+// const numscore = new Number (score);
+
+// console.log(numscore.toLocaleString('en-IN'));
+
+// //console.log(numscore.valueOf());
+
+// let myname = 123456
+
+// let fullname = new Number (myname);
+
+// console.log(fullname.toString().length);
+
+// maths start//
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(7.99));
+// console.log(Math.ceil(8.9));
+// console.log(Math.floor(9.9));
+// console.log(Math.min(1,2,3,4,5,6));
+//console.log(Math.max(10,20,40));
+
+// console.log(Math.random());
+
+// console.log((Math.random() * 10) + 1);
+
+
+let min = 20;
+let max = 30;
+
+let randomno = (Math.floor((Math.random() + 1) * (min)))
+
+console.log(randomno);
+
+//matchs end//
